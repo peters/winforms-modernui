69dc8679a687217829ffac8eb5a0c58cafb9d0e3
dennismagno <dimagnoph@yahoo.com>
2013-09-18 13:36:29 +0400

    MetroContextMenu
    Created additional control for MetroContextMenu

---
+++ MetroFramework/Controls/MetroContextMenu.cs
@@ -0,0 +1,212 @@
+﻿using MetroFramework;
+using MetroFramework.Components;
+using MetroFramework.Drawing;
+using MetroFramework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MetroFramework.Controls
+{
+    public class MetroContextMenu : ContextMenuStrip, IMetroControl
+    {
+        #region Interface
+
+        [Category("Metro Appearance")]
+        public event EventHandler<MetroPaintEventArgs> CustomPaintBackground;
+        protected virtual void OnCustomPaintBackground(MetroPaintEventArgs e)
+        {
+            if (GetStyle(ControlStyles.UserPaint) && CustomPaintBackground != null)
+            {
+                CustomPaintBackground(this, e);
+            }
+        }
+
+        [Category("Metro Appearance")]
+        public event EventHandler<MetroPaintEventArgs> CustomPaint;
+        protected virtual void OnCustomPaint(MetroPaintEventArgs e)
+        {
+            if (GetStyle(ControlStyles.UserPaint) && CustomPaint != null)
+            {
+                CustomPaint(this, e);
+            }
+        }
+
+        [Category("Metro Appearance")]
+        public event EventHandler<MetroPaintEventArgs> CustomPaintForeground;
+        protected virtual void OnCustomPaintForeground(MetroPaintEventArgs e)
+        {
+            if (GetStyle(ControlStyles.UserPaint) && CustomPaintForeground != null)
+            {
+                CustomPaintForeground(this, e);
+            }
+        }
+
+        private MetroColorStyle metroStyle = MetroColorStyle.Default;
+        [Category("Metro Appearance")]
+        [DefaultValue(MetroColorStyle.Default)]
+        public MetroColorStyle Style
+        {
+            get
+            {
+                if (DesignMode || metroStyle != MetroColorStyle.Default)
+                {
+                    return metroStyle;
+                }
+
+                if (StyleManager != null && metroStyle == MetroColorStyle.Default)
+                {
+                    return StyleManager.Style;
+                }
+                if (StyleManager == null && metroStyle == MetroColorStyle.Default)
+                {
+                    return MetroColorStyle.Blue;
+                }
+
+                return metroStyle;
+            }
+            set { metroStyle = value; }
+        }
+
+        private MetroThemeStyle metroTheme = MetroThemeStyle.Default;
+        [Category("Metro Appearance")]
+        [DefaultValue(MetroThemeStyle.Default)]
+        public MetroThemeStyle Theme
+        {
+            get
+            {
+                if (DesignMode || metroTheme != MetroThemeStyle.Default)
+                {
+                    return metroTheme;
+                }
+
+                if (StyleManager != null && metroTheme == MetroThemeStyle.Default)
+                {
+                    return StyleManager.Theme;
+                }
+                if (StyleManager == null && metroTheme == MetroThemeStyle.Default)
+                {
+                    return MetroThemeStyle.Light;
+                }
+
+                return metroTheme;
+            }
+            set { metroTheme = value; }
+        }
+
+        private MetroStyleManager metroStyleManager = null;
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public MetroStyleManager StyleManager
+        {
+            get { return metroStyleManager; }
+            set { 
+                metroStyleManager = value;
+                settheme();
+            }
+        }
+
+        private bool useCustomBackColor = false;
+        [DefaultValue(false)]
+        [Category("Metro Appearance")]
+        public bool UseCustomBackColor
+        {
+            get { return useCustomBackColor; }
+            set { useCustomBackColor = value; }
+        }
+
+        private bool useCustomForeColor = false;
+        [DefaultValue(false)]
+        [Category("Metro Appearance")]
+        public bool UseCustomForeColor
+        {
+            get { return useCustomForeColor; }
+            set { useCustomForeColor = value; }
+        }
+
+        private bool useStyleColors = false;
+        [DefaultValue(false)]
+        [Category("Metro Appearance")]
+        public bool UseStyleColors
+        {
+            get { return useStyleColors; }
+            set { useStyleColors = value; }
+        }
+
+        [Browsable(false)]
+        [Category("Metro Behaviour")]
+        [DefaultValue(false)]
+        public bool UseSelectable
+        {
+            get { return GetStyle(ControlStyles.Selectable); }
+            set { SetStyle(ControlStyles.Selectable, value); }
+        }
+
+        #endregion
+
+        
+        public MetroContextMenu(IContainer Container) {
+             if (Container != null)
+            {
+                Container.Add(this);
+            }
+        }
+
+        private void settheme()
+        {
+            this.BackColor = MetroPaint.BackColor.Form(Theme);
+            this.ForeColor = MetroPaint.ForeColor.Button.Normal(Theme);            
+            this.Renderer = new MetroCTXRenderer(Theme, Style);     
+        }
+
+        private class MetroCTXRenderer : ToolStripProfessionalRenderer
+        {
+            public MetroCTXRenderer(MetroFramework.MetroThemeStyle Theme, MetroColorStyle Style) : base(new contextcolors(Theme, Style)) { }
+        }
+
+        private class contextcolors : ProfessionalColorTable
+        {
+            MetroThemeStyle _theme = MetroThemeStyle.Light;
+            MetroColorStyle _style = MetroColorStyle.Blue;
+
+            public contextcolors(MetroFramework.MetroThemeStyle Theme, MetroColorStyle Style)
+            {
+                _theme = Theme;
+                _style = Style;
+            }
+
+            public override Color MenuItemSelected
+            {
+                get { return MetroPaint.GetStyleColor(_style); }
+            }
+
+            public override Color MenuBorder
+            {
+                get { return MetroPaint.BackColor.Form(_theme); }
+            }
+
+            public override Color MenuItemBorder
+            {
+                get { return MetroPaint.GetStyleColor(_style); }
+            }
+
+            public override Color ImageMarginGradientBegin
+            {
+                get { return MetroPaint.BackColor.Form(_theme); }
+            }
+
+            public override Color ImageMarginGradientMiddle
+            {
+                get { return MetroPaint.BackColor.Form(_theme); }
+            }
+
+            public override Color ImageMarginGradientEnd
+            {
+                get { return MetroPaint.BackColor.Form(_theme); }
+            }
+        }
+    }
+}

2780b566a37f6c1073fe5f3728e6db83cc2d8aa2
Dennis Magno <dimagnoph@gmail.com>
2015-08-26 01:00:38 +0800

    MetroLink Image
    Allow adding image to MetroLink

--- MetroFramework.Design/Controls/MetroLinkDesigner.cs
+++ MetroFramework.Design/Controls/MetroLinkDesigner.cs
@@ -37,28 +37,28 @@ namespace MetroFramework.Design.Controls
             }
         }
 
-        protected override void PreFilterProperties(IDictionary properties)
-        {
-            properties.Remove("ImeMode");
-            properties.Remove("Padding");
-            properties.Remove("FlatAppearance");
-            properties.Remove("FlatStyle");
-            properties.Remove("AutoEllipsis");
-            properties.Remove("UseCompatibleTextRendering");
+protected override void PreFilterProperties(IDictionary properties)
+{
+    properties.Remove("ImeMode");
+    properties.Remove("Padding");
+    properties.Remove("FlatAppearance");
+    properties.Remove("FlatStyle");
+    properties.Remove("AutoEllipsis");
+    properties.Remove("UseCompatibleTextRendering");
 
-            properties.Remove("Image");
-            properties.Remove("ImageAlign");
-            properties.Remove("ImageIndex");
-            properties.Remove("ImageKey");
-            properties.Remove("ImageList");
-            properties.Remove("TextImageRelation");
+    //properties.Remove("Image");
+    //properties.Remove("ImageAlign");
+    properties.Remove("ImageIndex");
+    properties.Remove("ImageKey");
+    properties.Remove("ImageList");
+    properties.Remove("TextImageRelation");
 
-            properties.Remove("UseVisualStyleBackColor");
+    properties.Remove("UseVisualStyleBackColor");
 
-            properties.Remove("Font");
-            properties.Remove("RightToLeft");
+    properties.Remove("Font");
+    properties.Remove("RightToLeft");
 
-            base.PreFilterProperties(properties);
-        }
+    base.PreFilterProperties(properties);
+}
     }
 }
--- MetroFramework/Controls/MetroLink.cs
+++ MetroFramework/Controls/MetroLink.cs
@@ -29,6 +29,7 @@ using System.Windows.Forms;
 using MetroFramework.Components;
 using MetroFramework.Drawing;
 using MetroFramework.Interfaces;
+using System.Drawing.Imaging;
 
 namespace MetroFramework.Controls
 {
@@ -175,6 +176,62 @@ namespace MetroFramework.Controls
             set { SetStyle(ControlStyles.Selectable, value); }
         }
 
+private Image _image = null;
+[DefaultValue(null)]
+[Category(MetroDefaults.PropertyCategory.Appearance)]
+public virtual new Image Image
+{
+    get { return _image; }
+    set
+    {
+        _image = value;
+        createimages();
+    }
+}
+
+
+private Image _nofocus = null;
+[DefaultValue(null)]
+[Category(MetroDefaults.PropertyCategory.Appearance)]
+public Image NoFocusImage
+{
+    get { return _nofocus; }
+    set { _nofocus = value; }
+}
+
+
+Int32 _imagesize = 16;
+
+[DefaultValue(16)]
+[Category(MetroDefaults.PropertyCategory.Appearance)]
+public Int32 ImageSize
+{
+    get { return _imagesize; }
+    set
+    {
+        _imagesize = value;
+        Invalidate();
+    }
+}
+
+        public override string Text
+        {
+            get
+            {
+                return base.Text;
+            }
+            set
+            {
+                base.Text = value;
+
+                if (AutoSize && _image != null)
+                {
+                    base.Width = TextRenderer.MeasureText(value, MetroFonts.Link(metroLinkSize, metroLinkWeight)).Width;
+                    base.Width += _imagesize + 2;
+                }
+            }
+        }
+
         #endregion
 
         #region Fields
@@ -225,7 +282,6 @@ namespace MetroFramework.Controls
                      ControlStyles.ResizeRedraw |
                      ControlStyles.UserPaint, true);
         }
-
         #endregion
 
         #region Paint Methods
@@ -275,17 +331,18 @@ namespace MetroFramework.Controls
             }
         }
 
+        Color foreColor;
+
         protected virtual void OnPaintForeground(PaintEventArgs e)
         {
-            Color foreColor;
-
             if (useCustomForeColor)
                 foreColor = ForeColor;
             else
             {
                 if (isHovered && !isPressed && Enabled)
                 {
-                    foreColor = MetroPaint.ForeColor.Link.Hover(Theme);
+                    //foreColor = MetroPaint.ForeColor.Link.Hover(Theme);
+                    foreColor = MetroPaint.ForeColor.Link.Normal(Theme);
                 }
                 else if (isHovered && isPressed && Enabled)
                 {
@@ -297,7 +354,7 @@ namespace MetroFramework.Controls
                 }
                 else
                 {
-                    foreColor = !useStyleColors ? MetroPaint.ForeColor.Link.Normal(Theme) : MetroPaint.GetStyleColor(Style);
+                    foreColor = !useStyleColors ? MetroPaint.ForeColor.Link.Hover(Theme) : MetroPaint.GetStyleColor(Style);
                 }
             }
 
@@ -307,8 +364,145 @@ namespace MetroFramework.Controls
 
             if (displayFocusRectangle && isFocused)
                 ControlPaint.DrawFocusRectangle(e.Graphics, ClientRectangle);
+
+            if (_image != null) DrawIcon(e.Graphics);
         }
 
+private void DrawIcon(Graphics g)
+{
+    if (Image != null)
+    {
+        int _imgW = _imagesize;
+        int _imgH = _imagesize;
+
+        if (_imagesize == 0)
+        {
+            _imgW = _image.Width;
+            _imgH = _image.Height;
+        }
+
+        Point iconLocation = new Point(2, (ClientRectangle.Height - _imagesize) / 2);
+        int _filler = 0;
+
+        switch (ImageAlign)
+        {
+            case ContentAlignment.BottomCenter:
+                iconLocation = new Point((ClientRectangle.Width - _imgW) / 2, (ClientRectangle.Height - _imgH) - _filler);
+                break;
+            case ContentAlignment.BottomLeft:
+                iconLocation = new Point(_filler, (ClientRectangle.Height - _imgH) - _filler);
+                break;
+            case ContentAlignment.BottomRight:
+                iconLocation = new Point((ClientRectangle.Width - _imgW) - _filler, (ClientRectangle.Height - _imgH) - _filler);
+                break;
+            case ContentAlignment.MiddleCenter:
+                iconLocation = new Point((ClientRectangle.Width - _imgW) / 2, (ClientRectangle.Height - _imgH) / 2);
+                break;
+            case ContentAlignment.MiddleLeft:
+                iconLocation = new Point(_filler, (ClientRectangle.Height - _imgH) / 2);
+                break;
+            case ContentAlignment.MiddleRight:
+                iconLocation = new Point((ClientRectangle.Width - _imgW) - _filler, (ClientRectangle.Height - _imgH) / 2);
+                break;
+            case ContentAlignment.TopCenter:
+                iconLocation = new Point((ClientRectangle.Width - _imgW) / 2, _filler);
+                break;
+            case ContentAlignment.TopLeft:
+                iconLocation = new Point(_filler, _filler);
+                break;
+            case ContentAlignment.TopRight:
+                iconLocation = new Point((ClientRectangle.Width - _imgW) - _filler, _filler);
+                break;
+        }
+
+        iconLocation.Y += 1;
+
+        if (_nofocus == null)
+        {
+            if (Theme == MetroThemeStyle.Dark)
+            {
+                g.DrawImage((isHovered && !isPressed) ? _darkimg : _darklightimg, new Rectangle(iconLocation, new Size(_imgW, _imgH)));
+            }
+            else
+            {
+                g.DrawImage((isHovered && !isPressed) ? _lightimg : _lightlightimg, new Rectangle(iconLocation, new Size(_imgW, _imgH)));
+            }
+        }
+        else
+        {
+            if (Theme == MetroThemeStyle.Dark)
+            {
+                g.DrawImage((isHovered && !isPressed) ? _darkimg : _nofocus, new Rectangle(iconLocation, new Size(_imgW, _imgH)));
+            }
+            else
+            {
+                g.DrawImage((isHovered && !isPressed) ? _image : _nofocus, new Rectangle(iconLocation, new Size(_imgW, _imgH)));
+            }
+        }
+    }
+}
+
+Image _lightlightimg = null;
+Image _darklightimg = null;
+Image _lightimg = null;
+Image _darkimg = null;
+
+private void createimages()
+{
+    if (_image != null)
+    {
+        _lightimg = _image;
+        _darkimg = ApplyInvert(new Bitmap(_image));
+
+        _darklightimg = ApplyLight(new Bitmap(_darkimg));
+        _lightlightimg = ApplyLight(new Bitmap(_lightimg));
+    }
+}
+public Bitmap ApplyInvert(Bitmap bitmapImage)
+{
+    byte A, R, G, B;
+    Color pixelColor;
+
+    for (int y = 0; y < bitmapImage.Height; y++)
+    {
+        for (int x = 0; x < bitmapImage.Width; x++)
+        {
+            pixelColor = bitmapImage.GetPixel(x, y);
+            A = pixelColor.A;
+            R = (byte)(255 - pixelColor.R);
+            G = (byte)(255 - pixelColor.G);
+            B = (byte)(255 - pixelColor.B);
+            bitmapImage.SetPixel(x, y, Color.FromArgb((int)A, (int)R, (int)G, (int)B));
+        }
+    }
+
+    return bitmapImage;
+}
+
+public Bitmap ApplyLight(Bitmap bitmapImage)
+{
+    byte A, R, G, B;
+    Color pixelColor;
+
+    for (int y = 0; y < bitmapImage.Height; y++)
+    {
+        for (int x = 0; x < bitmapImage.Width; x++)
+        {
+            pixelColor = bitmapImage.GetPixel(x, y);
+
+            A = pixelColor.A;
+            if (pixelColor.A <= 255 && pixelColor.A >= 100)
+            { A = 90; }
+
+            R = (byte)(pixelColor.R);
+            G = (byte)(pixelColor.G);
+            B = (byte)(pixelColor.B);
+            bitmapImage.SetPixel(x, y, Color.FromArgb((int)A, (int)R, (int)G, (int)B));
+        }
+    }
+
+    return bitmapImage;
+}
         #endregion
 
         #region Focus Methods
@@ -316,6 +510,8 @@ namespace MetroFramework.Controls
         protected override void OnGotFocus(EventArgs e)
         {
             isFocused = true;
+            // isHovered = true;
+            isPressed = false;
             Invalidate();
 
             base.OnGotFocus(e);
@@ -334,6 +530,8 @@ namespace MetroFramework.Controls
         protected override void OnEnter(EventArgs e)
         {
             isFocused = true;
+            //isHovered = true;
+            isPressed = true;
             Invalidate();
 
             base.OnEnter(e);
@@ -367,8 +565,11 @@ namespace MetroFramework.Controls
 
         protected override void OnKeyUp(KeyEventArgs e)
         {
-            isHovered = false;
-            isPressed = false;
+            if (!isFocused)
+            {
+                isHovered = false;
+                isPressed = false;
+            }
             Invalidate();
 
             base.OnKeyUp(e);
@@ -392,6 +593,9 @@ namespace MetroFramework.Controls
             {
                 isPressed = true;
                 Invalidate();
+
+                if (Name == "lnkClear" && Parent.GetType().Name == "MetroTextBox") this.PerformClick();
+                if (Name == "lnkClear" && Parent.GetType().Name == "SearchControl") this.PerformClick();
             }
 
             base.OnMouseDown(e);
@@ -408,6 +612,8 @@ namespace MetroFramework.Controls
         protected override void OnMouseLeave(EventArgs e)
         {
             isHovered = false;
+            isPressed = false;
+
             Invalidate();
 
             base.OnMouseLeave(e);

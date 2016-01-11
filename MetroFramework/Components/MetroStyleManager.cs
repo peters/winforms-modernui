554edb726e7e0b39039f8944e94086379a136422
Dennis Magno <dimagnoph@gmail.com>
2016-01-11 20:22:05 +0800

    Fix issue with MetroStyleManager

--- MetroFramework/Components/MetroStyleManager.cs
+++ MetroFramework/Components/MetroStyleManager.cs
@@ -27,6 +27,7 @@ using System.Diagnostics;
 using System.Windows.Forms;
 using MetroFramework.Controls;
 using MetroFramework.Interfaces;
+using System.Reflection;
 
 namespace MetroFramework.Components
 {
@@ -144,6 +145,26 @@ namespace MetroFramework.Components
             if (owner is IMetroForm)
             {
                 clonedManager.Owner = owner;
+                ((IMetroForm)owner).StyleManager = clonedManager;
+
+                Type parentForm = owner.GetType();
+                FieldInfo fieldInfo = parentForm.GetField("components",
+                BindingFlags.Instance |
+                     BindingFlags.NonPublic);
+                IContainer mother = (IContainer)fieldInfo.GetValue(owner);
+                // Check for a helper component
+                foreach (Component obj in mother.Components)
+                {
+                    if (obj is IMetroComponent)
+                    {
+                        ApplyTheme((IMetroComponent)obj);
+                    }
+
+                    if (obj.GetType() == typeof(MetroContextMenu))
+                    {
+                        ApplyTheme((MetroContextMenu)obj);
+                    }
+                }
             }
 
             return clonedManager;
